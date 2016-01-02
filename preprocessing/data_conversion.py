51863491ca789621b89041a4b47ac9f6ccb32b50
Jan Gillich <jan.gillich@tink.se>
2016-01-02 19:30:02 +0100

    alter

--- preprocessing/data_conversion.py
+++ preprocessing/data_conversion.py
@@ -11,12 +11,12 @@ csv2 = "../csv/indikatorenatlas2014bevoelkerungeinpersonenhaushalte.csv"
 csv3 = "../csv/indikatorenatlas2014bevoelkerungmittlerelebenserwartung.csv"
 csv4 = "../csv/indikatorenatlas2014bevoelkerungeinwohnerdichte.csv"
 csv5 = "../csv/indikatorenatlas2014arbeitsmarktarbeitslose.csv"
+csv6 = "../csv/indikatorenatlas2014bevoelkerungaltersdurchschnitt.csv"
 
 dtype = [('ap', '|S50'), ('value', '|S20'), ('jahr', '|S10'), ('id', '|S10'), ('name', '|S50')]
 np.set_printoptions(threshold=np.nan)
 
 auslaender = np.loadtxt(csv, dtype=dtype, delimiter='","', comments='', usecols=(2,3,14,16,17), skiprows=0)
-
 auslaender = f.datasetPreprocessing(auslaender)
 
 einpersonen = np.loadtxt(csv2, dtype=dtype, delimiter='","', comments='', usecols=(2,3,14,16,17), skiprows=0)
@@ -34,13 +34,17 @@ arbeitslosenquote = np.loadtxt(csv5, dtype=dtype, delimiter='","', comments='', 
 arbeitslosenquote = arbeitslosenquote[np.where(arbeitslosenquote['ap'] == 'Vollzeitarbeitssuchende')]
 arbeitslosenquote = f.datasetPreprocessing(arbeitslosenquote)
 
-data = np.zeros((auslaender['id'].size,5))
+alter = np.loadtxt(csv6, dtype=dtype, delimiter='","', comments='', usecols=(2,3,14,16,17), skiprows=0)
+alter = alter[np.where(alter['ap'] == 'gesamt')]
+alter = f.datasetPreprocessing(alter)
+
+data = np.zeros((auslaender['id'].size,6))
 data[:,0] = auslaender['value']
 data[:,1] = einpersonen['value']
 data[:,2] = lebenserwartung['value']
 data[:,3] = dichte['value']
 data[:,4] = arbeitslosenquote['value']
-
+data[:,5] = alter['value']
 #zur kontrolle
 #newdtype = [('id1', '|S10'), ('jahr1', 'i2'), ('value1', 'f8'), ('id2', '|S10'), ('jahr2', 'i2'), ('value2', 'f8')]
 #new_struc = np.zeros(auslaender['id'].size, newdtype)
@@ -51,7 +55,7 @@ data[:,4] = arbeitslosenquote['value']
 #new_struc['value1'] = auslaender['value']
 #new_struc['value2'] = einpersonen['value']
 
-#print data
+print data
 
 for col1 in xrange(0,data[0,:].size):
 	for col2 in xrange(0,data[0,:].size):
